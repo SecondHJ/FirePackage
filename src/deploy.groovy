7903389be54880b85f0b5870933f01daf109657e
jianghuajun <mayun153667>
2021-12-24 11:11:24 +0800

    init commit

--- src/deploy.groovy
+++ src/deploy.groovy
@@ -3,8 +3,10 @@ import groovy.json.JsonSlurper
 @Grab('com.aestasit.infrastructure.sshoogr:sshoogr:0.9.28')
 import static com.aestasit.infrastructure.ssh.DefaultSsh.*
 
+// load server config
+configs = yaml.load(new File('../resources/config.yaml').text)
 // module name
-def project = /Module name/
+def module = configs.module.name
 
 // local jar path
 def local_path = new JsonSlurper().parseText(new File('../resources/local.json').text)
@@ -12,12 +14,10 @@ def local_path = new JsonSlurper().parseText(new File('../resources/local.json')
 def remote_path = new JsonSlurper().parseText(new File('../resources/remote.json').text)
 
 def jar_name = {
-    paths = local_path[project].split('\\\\')
+    paths = local_path[module].split('\\\\')
     projectName = paths[paths.length - 1]
 }
 
-// load server config
-configs = yaml.load(new File('../resources/config.yaml').text)
 def host = configs.server.host
 def port = configs.server.port
 def username = configs.server.username
@@ -26,7 +26,7 @@ def password = configs.server.password
 // upload
 options.trustUnknownHosts = true
 remoteSession('root:$password@$host:$port') {
-    exec 'scp -r ' + local_path[project] + 'root@$host:$port:' + remote_path[project]
+    exec 'scp -r ' + local_path[module] + 'root@$host:$port:' + remote_path[project]
 }
 
 
